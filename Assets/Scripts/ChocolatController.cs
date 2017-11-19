d1ac06296eece8788524a9274bd728f65d0856dd
Vincent <vinc_proulx@hotmail.com>
2017-11-19 14:19:09 -0500

    Jump Completed

--- Assets/Scripts/ChocolatController.cs
+++ Assets/Scripts/ChocolatController.cs
@@ -3,56 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChocolatController : MonoBehaviour {
-	
-	public float speed = 15.0F;
-	public float jumpSpeed = 5.5F;
-	public Vector3 gravity = new Vector3(0.0F,-20.0F ,0.0F);
 
-	private Vector3 velocity;
-	private CharacterController cc;
-	private GameObject parent;
-	private PlayerController pc;
+	public float speed = 10f;
+	public float jump = 5f;
+
+	public LayerMask whatIsWagon;
+	private Transform wagonCheck;
+	private bool isOnWagon;
+
+	private Rigidbody rb;
+
 
 	void Awake(){
-		cc = GetComponent<CharacterController> ();
-		velocity = new Vector3(speed,0.0F,0.0F);
-		parent = GameObject.FindGameObjectWithTag("Player");
-
+		rb = GetComponent<Rigidbody>();
+		wagonCheck = this.transform.Find ("wagonCheck");
 	}
 
-	void Update(){
 
-
+	void FixedUpdate(){
+		isOnWagon = IsOnWagon ();
+		SetMass ();
+		SetSpeed ();
+		CheckJump ();
 	}
 
-	void FixedUpdate()
+
+	private void SetSpeed(){
+		rb.velocity = new Vector3 (speed, rb.velocity.y, rb.velocity.z);
+	}
+
+	private void SetMass(){
+		rb.mass = (isOnWagon) ? 0.0000001f : 1f;
+	}
+
+	private void CheckJump()
 	{
-		
+		if (isOnWagon)
+		{
+			if (Input.GetKey(KeyCode.E))
+			{
+				rb.mass = 1f;
+				rb.AddForce(new Vector3(0f, jump, 0f), ForceMode.Impulse);
+			}
+		}
+	}
 
+	private bool IsOnWagon(){
+		return Physics.CheckSphere (wagonCheck.position, 0.05f ,whatIsWagon.value);
 	}
 
 }
-
-/*
-		bool isGrounded = ((cc.collisionFlags & CollisionFlags.Below) != 0) ? true : false;
-		if (isGrounded)
-		{
-			transform.SetParent(parent.transform);
-			if (Input.GetKey(KeyCode.E))
-			{
-				transform.parent = null;
-				cc.Move(velocity * Time.deltaTime);
-				velocity = new Vector3(speed,0.0F,0.0F);
-				velocity.y += jumpSpeed;
-			}
-		}
-		if (!isGrounded) 
-		{
-			velocity += gravity * Time.deltaTime;
-			cc.Move(velocity * Time.deltaTime);
-		}
-
-		cc.Move(velocity * Time.deltaTime);
-		//Debug.Log ("x: " + velocity.x* Time.deltaTime + ", y: " + velocity.y* Time.deltaTime + ", z: "+ velocity.z* Time.deltaTime);
-		//Debug.Log(transform.IsChildOf (parent.transform));
-*/
---
+++ Assets/Scripts/WagonController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WagonController : MonoBehaviour {
+
+
+	public float speed = 10f;
+	public float jump = 7f;
+
+	public LayerMask whatIsGround;
+	private Transform groundCheck;
+
+	private Rigidbody rb;
+
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		groundCheck = this.transform.Find ("groundCheck");
+	}
+
+	void FixedUpdate(){
+		SetSpeed ();
+		CheckJump ();
+	}
+
+
+	private void SetSpeed(){
+		rb.velocity = new Vector3 (speed, rb.velocity.y, rb.velocity.z);
+	}
+
+
+	private void CheckJump()
+	{
+		if (isGrounded())
+		{
+			if (Input.GetButton("Jump"))
+			{
+				rb.AddForce(new Vector3(0f, jump, 0f), ForceMode.Impulse);
+			}
+		}
+	}
+
+	private bool isGrounded(){
+		return Physics.CheckSphere (groundCheck.position, 0.05f ,whatIsGround.value);
+	}
+}
