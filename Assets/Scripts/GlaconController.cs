cf2dac91a8dd7a0b49f5629945823a4622d51742
Phil <phil.gatien@videotron.ca>
2017-11-29 20:44:06 -0500

    Glacon audio "pop"

--- Assets/Scripts/GlaconController.cs
+++ Assets/Scripts/GlaconController.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GlaconController : MonoBehaviour {
+
+	private AudioSource audio;
+	private GameObject parent;
+
+	void Awake(){
+		parent = this.transform.parent.gameObject;
+		audio = parent.GetComponent<AudioSource>();
+	}
 	
 	void OnCollisionEnter(Collision other)
 	{
 		if (other.gameObject.CompareTag ("Wagon") || other.gameObject.CompareTag ("Chocolat"))
 		{
+			audio.Play ();
 			this.gameObject.SetActive (false);
+
 		}
 	}
 }
